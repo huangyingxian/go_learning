ae085d97b6c525a0109dc1b1420fb88290e5139b
yingxian huang <yingxianhuang@MacBook-Pro-3.local>
2022-01-16 14:54:43 +0800

    复习数组

--- code/ch6/array_test/array_test.go
+++ code/ch6/array_test/array_test.go
@@ -1,6 +1,10 @@
 package array_test
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"testing"
+)
 
 func TestArrayInit(t *testing.T) {
 	var arr [3]int
@@ -16,13 +20,19 @@ func TestArrayTravel(t *testing.T) {
 	for i := 0; i < len(arr3); i++ {
 		t.Log(arr3[i])
 	}
-	for _, e := range arr3 {
-		t.Log(e)
+	for i, e := range arr3 { // 数组使用 range 返回其中的index 和对应的value
+		t.Log(i, e)
 	}
 }
 
 func TestArraySection(t *testing.T) {
 	arr3 := [...]int{1, 2, 3, 4, 5}
 	arr3_sec := arr3[:]
-	t.Log(arr3_sec)
+	log.Print(fmt.Sprintf("arr3_sec type:%T", arr3_sec))
+	t.Log(arr3_sec) // [1 2 3 4 5]
+
+	arr3[0] = 9
+	t.Log(arr3_sec) // [9 2 3 4 5]
+	t.Log(arr3)     // [9 2 3 4 5]  这里表明，使用 array[:] 得到的数组是原来数组的引用或者说是指向，操作的是同一份数据
+
 }
